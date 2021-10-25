f754afecd10aaea4b1daba61aa843841e81b9fe2
Agnelo Schmitt Neto <agneloschmittneto@gmail.com>
2021-10-25 14:45:15 -0300

    tratando erros com excessao personalizada

--- tabuleiro/Tabuleiro.cs
+++ tabuleiro/Tabuleiro.cs
@@ -1,14 +1,13 @@
 ﻿using System;
 
+
 namespace tabuleiro
 {
     class Tabuleiro
     {
         public int linhas { get; set; }
         public int colunas { get; set; }
-
         private Peca[,] pecas;
-
         public Tabuleiro(int linhas, int colunas)
         {
             this.linhas = linhas;
@@ -21,11 +20,42 @@ namespace tabuleiro
             return pecas[linha, coluna];
         }
 
+        public Peca peca(Posicao pos)
+        {
+            return pecas[pos.linha, pos.coluna];
+        }
+
+        public bool existePeca(Posicao pos)
+        {
+            validarPosicao(pos);
+            return peca(pos) != null;
+        }
+
         public void colocarPeca(Peca p, Posicao pos)
         {
+            if (existePeca(pos))
+            {
+                throw new TabuleiroException("Ja existe uma peça na posição!");
+            }
             pecas[pos.linha, pos.coluna] = p;
             p.posicao = pos;
         }
+        
+        public bool posicaoValida(Posicao pos)
+        {
+            if (pos.linha<0 ||pos.linha>=linhas || pos.coluna<0 || pos.coluna>=colunas)
+            {
+                return false;
+            }
+            return true;
+        }
 
+        public void validarPosicao(Posicao pos)
+        {
+            if (!posicaoValida(pos))
+            {
+                throw new TabuleiroException("Posição inválida!");
+            }
+        }
     }
 }
